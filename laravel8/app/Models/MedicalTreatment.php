737a9d507f2935e4a5f489cd077c9f6e7e982f4e
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-03-31 16:53:29 -0500

    added TestController and MedicalTreatment model

---
+++ laravel8/app/Models/MedicalTreatment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalTreatment extends Model
+{
+    use HasFactory;
+
+    // all
+    // find($id)
+    // save($params)
+    // update($params)
+    // create($params)
+    // delete($id)
+    // find()->where($clause)
+}
